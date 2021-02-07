11a7b1d259b2a92c4ce99092d836132d4209b893
Eliza <eliza.ksajikyan.h@gmail.com>
2021-02-07 23:03:54 +0400

    Fixed dropdown

--- src/Layouts/NotificationBlock/NotificationBlockItem/NotificationBlockItem.js
+++ src/Layouts/NotificationBlock/NotificationBlockItem/NotificationBlockItem.js
@@ -6,18 +6,18 @@ import './NotificationBlockItem.scss';
 
 function NotificationBlockItem() {
     return (
-        <NavDropdown.Item href="#" className="d-flex notification-item p-0 bg-transparent align-items-start">
+        <div className="d-flex notification-item p-0 bg-transparent align-items-start">
             <a href="" className="flex-shrink-0  notification-item-image overflow-hidden rounded-circle text-decoration-none">
                 <img src={profileImage} className="w-100 h-100" alt=""/>
             </a>
             <div className="flex-grow-1 notification-item-content">
                 <a href="" className="notification-item-user-name text-decoration-none d-inline-flex">Иванова Елена</a>
-                <p className="notification-item-info m-0">Нравится Ваша публикация <span className="notification-item-content-time">2 часа</span></p>
+                <a href="#" className="d-block notification-item-info m-0">Нравится Ваша публикация <span className="notification-item-content-time">2 часа</span></a>
             </div>
-            <figure className="flex-shrink-0 notification-item-image rounded overflow-hidden m-0">
+            <a href="#" className="flex-shrink-0 notification-item-image rounded overflow-hidden m-0">
                 <img src={profileImage} className="w-100 h-100" alt=""/>
-            </figure>
-        </NavDropdown.Item>
+            </a>
+        </div>
     );
 }
 
